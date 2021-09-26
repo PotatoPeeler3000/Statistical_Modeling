319a6243341aeaa4cf9740513d0104ed60b158ea
BirdBrain2000 <nkitchel2000@outlook.com>
2021-09-26 12:03:43 -0600

    Added Comments.

--- Dice Simulation.R
+++ Dice Simulation.R
@@ -1,13 +1,21 @@
+#Declare Varibles
 set.seed(444)
 nsim = 1e6
 e =rep(0,nsim)
+
+#Run simulation a million times
 for(i in 1:nsim)
 {
+  #Draw ten dice with replacement
   x = sample(1:6,10,replace = T)
   w = (diff(x) == 0)
   e[i] = sum(w, na.rm=TRUE)
+  
+  #Convert all consecutive values(diff == 0) to TRUE
   e[i] = e[i] == 0
 }
+
+#Probability over a million simulations
 probability = sum(e)/1e6
 print(probability)
 
