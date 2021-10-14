99016ba076b24a7d89b30996f621536e6ba4dd3b
BirdBrain2000 <nkitchel2000@outlook.com>
2021-10-14 14:59:22 -0600

    Program to simulate the expected value of draws to get 2 chocolates with raspberry filling when there are six chocolates in the box.

--- Chocolate Box.R
+++ Chocolate Box.R
@@ -1,11 +1,16 @@
-set.seed(432)
-
 #Define Values
-box = c(rep(0, 4), rep(1, 2))
+box = c(0, 0, 0, 0, 1, 1)
+k = c(2, 3, 4, 5, 6)
+storage = rep(0, 1e6)
 
+#Simulation loop
 for (i in 1:1e6)
 {
+  x = sample(box, 6)
   
-  
-  
+  storage[i] = (max(which(box == x)))
 }
+
+#Print expected outputs
+print(table(storage)/1e6)
+print(sum(k * ((k - 1)/ (factorial(6) / (factorial(2) * factorial(6 -2))))))
